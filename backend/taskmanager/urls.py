3473671dce68e933d9cf24d2fd2ada60ff58b803
Aananda-git <bhusal.aananda1@gmail.com>
2025-04-21 15:38:18 +0545

    fix: api end points

--- backend/taskmanager/urls.py
+++ backend/taskmanager/urls.py
@@ -34,7 +34,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
+    path('', include('core.urls')),
     path('api-token-auth/', obtain_auth_token),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
